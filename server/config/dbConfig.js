04d64686c2e8dfea13148cbd56447f01fca30db6
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-09 23:26:27 +0100

    DB created

---
+++ server/config/dbConfig.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MOGNO_URL)
+
+const connection = mongoose.connection
+
+mongoose.set('strictQuery', true)
+
+
+connection.on("connected", () => {
+    console.log("MongoDb successfully connected")
+})
+
+
+connection.on("error", () => {
+    console.log("Error on connectiong to MongoDB")
+})
+
+
+module.exports = mongoose
