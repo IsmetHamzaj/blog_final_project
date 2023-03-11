296548c002c58f6865a178aa282a5ed3689e987c
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-12 00:23:44 +0100

    Update auth.js

--- server/Auth/auth.js
+++ server/Auth/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET_KEY } = process.env;
+const JWT_SECRET_KEY = "hG7kL9a12jB4c6dE8f0iJ3mN5oP1qRtS"
+
+console.log(JWT_SECRET_KEY)
 
 function generateToken(userId) {
     const token = jwt.sign({ userId }, JWT_SECRET_KEY, { expiresIn: '1h' });
