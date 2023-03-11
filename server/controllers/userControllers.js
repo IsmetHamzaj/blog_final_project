ad4f917b24057a6c4c04e058e9c884e9cc66f782
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-12 00:37:04 +0100

    Update userControllers.js

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -57,18 +57,19 @@ const getUserById = async (req, res) => {
 
 const register = async (req, res) => {
     try {
-        const newUser = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }).then((createdUser) => {
-            res.status(200).json({
-                status: "Success",
-                success: true,
-                message: "Here is the data",
-                data: createdUser
-            })
-        })
+        const userExists = await User.findOne({ email: req.body.email })
+        if (userExists) {
+            return res
+                .status(200)
+                .send({ message: "User already exists", success: false })
+        }
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+        req.body.password = hashedPassword
+        const newUser = new User(req.body)
+        await newUser.save()
+        res.status(200).send({ message: "User created successfully", success: true })
     } catch (error) {
         res.status(500).json({
             status: "Failed",
