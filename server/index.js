d30090d5d4a8dc5a42cdfd3da97f54ad378dd297
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-11 23:32:05 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 
 const dbConfig = require('./config/dbConfig')
 const blogRoutes = require('./routes/blogRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,5 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use("/api/blogs", blogRoutes)
+app.use("/api/users", userRoutes)
 
 app.listen(3000, () => {console.log(`Server is running on port 3000`)})
