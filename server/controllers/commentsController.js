84d8b3d328441ed0b0fea7b25d21e5b87b2cf6d8
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-14 20:35:56 +0100

    Update commentsController.js

---
+++ server/controllers/commentsController.js
@@ -0,0 +1,55 @@
+const Comments = require('./../models/commentsModel')
+
+
+const getAllComments = async (req, res) => {
+    try {
+        const comments = await Comments.find()
+        if(comments.length > 0) {
+            res.status(200).json({
+                status: "Success",
+                success: true,
+                data: comments
+            })
+        } else {
+            res.status(404).json({
+                status: "Success",
+                message: "There are no comments"
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            status: "Failed",
+            success: false,
+            message: error
+        })
+    }
+}
+
+
+const getCommentById = async (req, res) => {
+    try {
+        const comment = await Comments.findById(req.params.id)
+        if(!comment) {
+            res.status(404).json({
+                status: "Success",
+                success: true,
+                message: "Comment not found"
+            })
+        } else {
+            res.json(200).json({
+                status: "Success",
+                success: true,
+                data: comment
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            status: "Failed",
+            success: false,
+            message: error
+        })
+    }
+}
+
+
+module.exports = {getAllComments, getCommentById}
