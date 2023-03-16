936e9bc16c052199f74b4679902c9a9cff05efcf
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-16 20:57:50 +0100

    Update store.js

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,2 +1,15 @@
-import {cretaStore} from 'redux'
+import {combineReducers} from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import {Provider} from "react-redux"
+import { loadingSlice } from './loadingSlice'
+
+const rootReducer = combineReducers({
+    loadings: loadingSlice.reducer
+})
+
+
+const store = configureStore({
+    reducer: rootReducer
+})
+
+export default store
