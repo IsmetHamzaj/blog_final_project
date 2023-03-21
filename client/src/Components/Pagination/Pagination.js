a7319a801645b2f0aec2d33fbc945ecd9911a7b4
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-21 20:26:17 +0100

    Update pagination

--- client/src/Components/Pagination/Pagination.js
+++ client/src/Components/Pagination/Pagination.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const Pagination = () => {
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
   return (
-    <div>Pagination</div>
-  )
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map(page => (
+          <li key={page} className="page-item">
+            <a href="#" className={`page-link ${page === currentPage ? 'active' : ''}`} onClick={() => onPageChange(page)}>{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
-export default Pagination
+export default Pagination;
--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -2,36 +2,48 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { showLoading, hideLoading } from '../../Redux/loadingSlice'
+import Pagination from '../../Components/Pagination'
 
 const Home = () => {
   const dispatch = useDispatch()
-    const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-    useEffect(() => {
-        dispatch(showLoading())
-        axios.get("http://localhost:3000/api/blogs")
-        .then(res => {
-          setBlogs(res.data.data)
-        })
-        .catch(err => {
-          console.log(err)
-        })
-        dispatch(hideLoading())
-    }, [])
+  useEffect(() => {
+    dispatch(showLoading())
+    axios.get("http://localhost:3000/api/blogs")
+      .then(res => {
+        setBlogs(res.data.data)
+        setCurrentPage(res.currentPage);
+        setTotalPages(res.totalPages);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    dispatch(hideLoading())
+  }, [])
+
+
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  }
 
   return (
     <div>
-        {
-            blogs.map((blog) => {
-                return(
-                    <div key={blog._id}>
-                        <p>{blog.title}</p>
-                        <p>{blog.description}</p>
-                        <p>#{blog.tags}</p>
-                    </div>
-                )
-            })
-        }
+      {
+        blogs.map((blog) => {
+          return (
+            <div key={blog._id}>
+              <p>{blog.title}</p>
+              <p>{blog.description}</p>
+              <p>#{blog.tags}</p>
+            </div>
+          )
+        })
+      }
+      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
     </div>
   )
 }
