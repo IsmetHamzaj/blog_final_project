ecb268deb130958a9ff841f024e694383e7909ba
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-23 04:24:26 +0100

    Error

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,61 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { showLoading, hideLoading } from '../../Redux/loadingSlice'
-// import Pagination from '../../Components/Pagination'
 
 const Home = () => {
   const dispatch = useDispatch()
-  const [blogs, setBlogs] = useState([])
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const [BlogsPerPage, setBlogsPerPage] = useState(12)
+  const blogs = useSelector((state) => state.blog.blogs)
+  const loading = useSelector((state) => state.loading)
   const [currentPage, setCurrentPage] = useState(1)
-  const [BlogsPerPage, setBlogsPerPage] = useState(12) 
+  const [BlogsPerPage, setBlogsPerPage] = useState(12)
 
   console.log(BlogsPerPage)
   console.log(currentPage)
   useEffect(() => {
     dispatch(showLoading())
-    axios.get("http://localhost:3000/api/blogs")
-      .then(res => {
-        setBlogs(res.data.data)
-        setBlogsPerPage(res.data.totalPages)
-        setCurrentPage(res.data.currentPage)
-      })
-      .catch(err => {
+    const response = axios.get("http://localhost:3000/api/blogs")
+      .then(() => dispatch(hideLoading()))
+      .catch((err) => {
         console.log(err)
+        dispatch(hideLoading())
       })
-    dispatch(hideLoading())
   }, [])
-
-
-
-  // const indexOfLastBlog = currentPage * BlogsPerPage;
-  // const indexOfFirstBlog = indexOfLastBlog - BlogsPerPage
-  // const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog)
-
-
-  // const paginate = (pageNumber, e) => {
-  //   e.preventDefault()
-  //   setCurrentPage(pageNumber)
-  // }
-
 
   return (
     <div>
       {
-        blogs.map((blog) => {
-          return (
-            <div key={blog._id}>
-              <p>{blog.title}</p>
-              <p>{blog.description}</p>
-              <p>#{blog.tags}</p>
-            </div>
-          )
-        })
+        loading ? (
+          <p>Loading...</p>
+        ) : (
+          blogs.map((blog) => {
+            return (
+              <div key={blog._id}>
+                <p>{blog.title}</p>
+                <p>{blog.description}</p>
+                <p>#{blog.tags}</p>
+              </div>
+            )
+          })
+        )
       }
       <p>{currentPage}</p>
-      {/* <Pagination BlogsPerPage={BlogsPerPage} totalBlogs={blogs.length} paginate={paginate} currentPage={currentPage} /> */}
     </div>
   )
 }
