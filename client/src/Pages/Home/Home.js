15c54606e959a92ae6ecbd7ea94e0a96d73cbb36
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-23 04:51:22 +0100

    Update Home.js

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -2,25 +2,32 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { showLoading, hideLoading } from '../../Redux/loadingSlice'
+import blogSlice, { addBlog } from '../../Redux/blogSlice'
 
 const Home = () => {
   const dispatch = useDispatch()
-  const blogs = useSelector((state) => state.blog.blogs)
+  const blogs = useSelector((state) => state.blog.blog[0])
+  console.log(blogs)
   const loading = useSelector((state) => state.loading)
   const [currentPage, setCurrentPage] = useState(1)
   const [BlogsPerPage, setBlogsPerPage] = useState(12)
 
-  console.log(BlogsPerPage)
-  console.log(currentPage)
+
   useEffect(() => {
-    dispatch(showLoading())
-    const response = axios.get("http://localhost:3000/api/blogs")
-      .then(() => dispatch(hideLoading()))
-      .catch((err) => {
-        console.log(err)
-        dispatch(hideLoading())
+    console.log("Before fetching data, loading is:", loading);
+    dispatch(showLoading());
+    axios.get("http://localhost:3000/api/blogs")
+      .then((response) => {
+        const blogs = response.data.data;
+        dispatch(addBlog(blogs));
+        dispatch(hideLoading());
+        console.log("After fetching data, loading is:", loading);
       })
-  }, [])
+      .catch((error) => {
+        dispatch(hideLoading());
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div>
