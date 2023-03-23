634d764b43f47b3dc13b6a4641abb33913bb4721
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-23 04:14:47 +0100

    Update blogSlice.js

--- client/src/Redux/blogSlice.js
+++ client/src/Redux/blogSlice.js
@@ -1,5 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    blogs: [],
+    currentBlog: {}
+}
 
 export const blogSlice = createSlice({
-    
+    name: 'blog',
+    initialState,
+    reducers: {
+        addBlog: (state, action) => {
+            state.blogs.push(action.payload)
+        },
+        updateBlog: (state, action) => {
+            const index = state.blogs.findIndex(blog => blog._id === action.payload._id)
+            if(index !== -1) {
+                state.blogs[index] = action.payload
+            }
+        },
+        deleteBlog: (state, action) => {
+            state.blogs = state.blogs.filter(blog => blog._id !== action.payload)
+        },
+        setCurrentBlog: (state, action) {
+            
+        }
+    }
 })
