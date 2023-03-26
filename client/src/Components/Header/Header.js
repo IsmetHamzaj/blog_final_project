927a380ed2a976ee71bb854a8d0a7604f07e2cff
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-26 04:01:59 +0200

    Update Header.js

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -5,15 +5,32 @@ const Navbar = (props) => {
     const { match } = props
     return (
         <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to={`/profile/}`}>Profile</Link>
-                </li>
-            </ul>
-        </nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>Categories:</li>
+        <li>
+          <ul>
+            <li>
+              <Link to="/category/sports">Sports</Link>
+            </li>
+            <li>
+              <Link to="/category/politics">Politics</Link>
+            </li>
+            <li>
+              <Link to="/category/education">Education</Link>
+            </li>
+            <li>
+              <Link to="/category/technology">Technology</Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
     );
 };
 
