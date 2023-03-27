fbba833aa586ec9d06e5841ca9aeb50215e92aae
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-27 23:40:33 +0200

    Update Footer.js

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container">
+        <p>&copy; {new Date().getFullYear()} Your Company</p>
+        <div className="footer-social-icons">
+          <a href="#"><i className="fa fa-facebook"></i></a>
+          <a href="#"><i className="fa fa-twitter"></i></a>
+          <a href="#"><i className="fa fa-instagram"></i></a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
