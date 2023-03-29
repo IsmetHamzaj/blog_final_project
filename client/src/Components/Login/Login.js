38479c0292bbb66f792d179edf5ecc18889e149c
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-03-29 02:44:30 +0200

    Update Login.js

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -4,26 +4,32 @@ import { useNavigate } from 'react-router-dom'
 import './Login.css'
 import LayOut from '../LayOut'
 import toast from 'react-hot-toast'
+import { useDispatch } from 'react-redux'
+import { hideLoading, showLoading } from '../../Redux/loadingSlice'
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const onSubmit = async (e) => {
     e.preventDefault()
     try {
+      dispatch(showLoading())
       const userEmail = await e.target.email.value
       const userPassword = await e.target.password.value
       const response = await axios.post("http://localhost:3000/api/users", {
         email: userEmail,
         password: userPassword
       })
+      dispatch(hideLoading())
       if (response.data.data) {
         toast.success(response.data.success)
         toast("Redirecting to homepage")
         localStorage.setItem("token", response.data.data)
         navigate('/')
       } else {
+        dispatch(hideLoading())
         toast.error(response.data.message)
       }
     } catch (error) {
