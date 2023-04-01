8de9fe0870827f2ef2b95e1bca4963701e35a30b
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-04-01 03:01:28 +0200

    Bugs

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -15,7 +15,46 @@ const Home = () => {
   const loading = useSelector((state) => state.alerts)
   const [currentPage, setCurrentPage] = useState(1)
   const [BlogsPerPage, setBlogsPerPage] = useState(25)
+  useEffect(() => {
+    const com = [
+      {
+        id: 1,
+        description: "hiiidasdasdddddddddddddddddddddddddddddddd"
+      },
+      {
+        id: 2,
+        description: "hiiiidsaaaaaaaaaaaaaaaaaaaaa"
+      },
+      {
+        id: 3,
+        description: "hiiidasdasdasdasdasdasdasi"
+      }
+    ]
+    const data = setComments(com)
+  }, [])
 
+
+  const [comments, setComments] = useState([])
+  const [com, setCom] = useState(false)
+  console.log(comments)
+
+  function GetComments() {
+
+
+    return (
+      <div>
+        {
+          comments?.map((f) => {
+            return (
+              <div key={f.id}>
+                <p>{f.description}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 
   useEffect(() => {
     dispatch(showLoading());
@@ -46,20 +85,30 @@ const Home = () => {
           {loading ? <p>Loading...</p> : (
             <div className="blogs-grid">
               {blogs && currentBlogs?.map((blog) => (
-                <Link to={`/blogs/${blog._id}`} className="link-div">
-                  <div key={blog._id} className='blog-item'>
-                    <h2 className="blog-title">{blog.title}</h2>
-                    <p className="blog-description">{blog.description}</p>
-                    <div className="blog-tags">#{blog.tags}</div>
-                  </div>
-                </Link>
+                <div>
+                  <Link to={`/blogs/${blog._id}`} className="link-div">
+                    <div key={blog._id} className='blog-item'>
+                      <h2 className="blog-title">{blog.title}</h2>
+                      <p className="blog-description">{blog.description}</p>
+                      <div className="blog-tags">#{blog.tags}</div>
+                    </div>
+                  </Link>
+                  <button onClick={() => setCom(!com)}>See Comments</button>
+                  {
+                    com ? (
+                      <GetComments />
+                    ) : (
+                      null
+                    )
+                  }
+                </div>
               ))}
             </div>
           )}
         </div>
-        <div className="pagination">
-          <Pagination BlogsPerPage={BlogsPerPage} totalBlogs={blogs?.length} paginate={paginate} currentPage={currentPage} />
-        </div>
+      </div>
+      <div className="pagination">
+        <Pagination BlogsPerPage={BlogsPerPage} totalBlogs={blogs?.length} paginate={paginate} currentPage={currentPage} />
       </div>
     </div>
   );
