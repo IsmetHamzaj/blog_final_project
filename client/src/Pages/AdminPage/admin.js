f94394000539b5a217582681f804c52eea1c0178
IsmetHamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2023-04-11 03:28:19 +0200

    Admin panel

--- client/src/Pages/AdminPage/admin.js
+++ client/src/Pages/AdminPage/admin.js
@@ -1,14 +1,81 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import "./admin.css"
 
 const Admin = () => {
     const [adminUsers, setAdminUsers] = useState([])
+    const [adminBlogs, setAdminBlogs] = useState([])
     useEffect(() => {
-        axios.get("http://localhost:3000/")
+        axios.get("http://localhost:3000/api/blogs")
+            .then((res) => {
+                setAdminBlogs(res.data.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        axios.get("http://localhost:3000/api/admin/all-users")
+            .then((res) => {
+                setAdminUsers(res.data.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }, [])
-  return (
-    <div>admin</div>
-  )
+
+
+    function AllUsers() {
+        return (
+            <div >
+                <h3 className='title'>User Names:</h3>
+                <div className='admin-container'>
+                    {
+                        adminUsers.map((users) => {
+                            return (
+                                <div className='name_containers'>
+                                    <p className='names'>Name: {users.name}</p>
+                                    <p>Email: {users.email}</p>
+                                    <p>Password: {users.password}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+
+
+    function AllBlogs() {
+        return (
+            <div>
+                <h3 className='title'>User Blogs:</h3>
+                <div className='admin-container'>
+                    {
+                        adminBlogs.map((blogs) => {
+                            return (
+                                <div className='name_containers'>
+                                    <p>Title: {blogs.title}</p>
+                                    <p>Description: {blogs.description}</p>
+                                    <p>Date: {blogs.date}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+
+
+    return (
+        <div>
+            <AllUsers />
+            <hr></hr>
+            <hr></hr>
+            <hr></hr>
+            <AllBlogs />
+        </div>
+    )
 }
 
 export default Admin
